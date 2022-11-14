704e959dcef5a8991d6fd80fbc0244ecf7dad11f
gmgreer <beauski8@gmail.com>
2022-11-14 10:37:57 -0800

    alert index user story complete

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,14 @@
 import React from "react"
 
 const Square = (props) => {
+ 
+  const handleClick = () => {
+    props.handleGamePlay(props.index)
+  }
+
   return (
     <>
-      <div className="square">{props.item}</div>
+      <div className="square" onClick={handleClick}>{props.item}</div>
     </>
   )
 }
